2bcecde8e2211f30467abe61c9a470b75d28bed7
Ha Minh Vuong <Vuong@Has-iMac.local>
2016-10-31 23:03:19 +0700

    Add main file.

---
+++ gtoolinit.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+var tools = []string{
+	// "github.com/nsf/gocode",
+	// lint
+	"github.com/golang/lint/golint",
+	"github.com/kisielk/errcheck",
+	// format
+	"golang.org/x/tools/cmd/goimports",
+	"sourcegraph.com/sqs/goreturns",
+	// source analysis
+	"golang.org/x/tools/cmd/oracle",
+	"github.com/rogpeppe/godef",
+	"golang.org/x/tools/cmd/gorename",
+	// dependency graph
+	"github.com/kisielk/godepgraph",
+	// benchmark
+	"golang.org/x/tools/cmd/benchcmp",
+	"github.com/cespare/prettybench",
+	"github.com/ajstarks/svgo/benchviz",
+}
+
+func main() {
+
+	if len(os.Getenv("GOPATH")) == 0 {
+		fmt.Fprintln(os.Stderr, "GOPATH has not been set.")
+		os.Exit(1)
+	}
+
+	fmt.Println("Installing go tools ...")
+
+	cmd := "go"
+	args := []string{"get", "-u"}
+
+	for _, t := range tools {
+		targs := append(args, t)
+		if err := exec.Command(cmd, targs...).Run(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Printf("%v .√\n", t)
+	}
+
+}
